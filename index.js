ff77c0de76e95168f1b4dba98850153885ded60d
Michael Insalaco <mwi76@icloud.com>
2023-08-10 17:17:25 -0400

    finish js

--- index.js
+++ index.js
@@ -1,20 +1,50 @@
-// get info from input
-// get info from button
-
-// get info from error message
-
 const submitButton = document.getElementById("btn");
 
+const errorMessage = document.getElementById("errorMessage");
+
+const gallery = document.querySelector(".gallery");
+
 // fetch images from unspash.com
-function fetchImage () {
+async function fetchImage () {
   const inputValue = document.getElementById("input").value;
+
+  if (inputValue > 12 || inputValue  < 1) {
+    errorMessage.style.display = "block";
+    errorMessage.innerText = "number should be 1 through 12";
+    return
+  }
   
-  fetch(`https://api.unsplash.com/photos?per_page=${inputValue}&page=1&client_id=`).then(
-    (res) => 
-      res.json().then((data) => {
-        console.log(data);
-      })
-  );
+  let imgs = '';
+
+  try {
+    submitButton.style.display = "none";
+    const loading = `<img src="loading.svg" />`;
+    gallery.innerHTML = loading;
+  
+    await fetch(`https://api.unsplash.com/photos?per_page=${inputValue}&page=${Math.round(Math.random() * 1000)}&client_id=JrjhTJSxNLNPp1Vm9UmjZRHbzw8MA85dSBOdAtWNm0E`).then(
+      res => 
+        res.json().then(data => {
+
+          if (data) {
+            data.forEach(pic => {
+              imgs += `<img src="${pic.urls.small}" alt="image" />`;
+              gallery.style.display = "block";
+              gallery.innerHTML = imgs;
+              submitButton.style.display = "block";
+              errorMessage.style.display = "none";      
+            });
+          }
+        })
+    );
+  
+    
+
+  } catch (error) {
+    errorMessage.style.display = "block";
+    errorMessage.innerText = "An error has occurred. Try again later.";
+    submitButton.style.display = "block";
+    gallery.style.display = "none";
+  }
 }
 
 submitButton.addEventListener("click", fetchImage);
